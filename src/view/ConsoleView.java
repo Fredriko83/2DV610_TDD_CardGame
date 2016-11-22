813c568dd70c6c008df1774dac5f2e2f79b410a9
Fredrik Olsson <folep02@student.lnu.se>
2016-11-22 12:11:21 +0100

    RED ConsoleView - shouldShowMenu

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -1,6 +1,12 @@
 package view;
 
+import java.io.PrintStream;
+
 public class ConsoleView {
+
+	public ConsoleView(PrintStream printStream) {
+		// TODO Auto-generated constructor stub
+	}
 
 	public void showMenu() {
 		// TODO Auto-generated method stub		
---
+++ tests/view/ConsoleViewTest.java
@@ -0,0 +1,31 @@
+package view;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.PrintStream;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ConsoleViewTest {
+
+	private ConsoleView sut;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void shouldShowMenu() {
+		PrintStream printStream = Mockito.mock(PrintStream.class);
+		
+		sut = new ConsoleView(printStream);
+		sut.showMenu();
+		
+		verify(printStream).println();
+		
+	}
+
+}
