4b542f71d2702a371afa4448bde853a2b788e057
Fredrik Olsson <folep02@student.lnu.se>
2016-12-08 16:25:45 +0100

    REFACTOR changed the tests in DeckTest not to dependent on Card

--- tests/model/DealerTest.java
+++ tests/model/DealerTest.java
@@ -15,7 +15,7 @@ public class DealerTest {
 
 	
 	@Mock private Deck m_deck;
-	//@Mock private Card card = new Card(Card.Value.Ace, Card.Suite.Clubs);
+	@Mock private Card card;
 	@Mock private ArrayList<Card> m_hand;
 	
 	@InjectMocks private Dealer sut;
@@ -34,7 +34,7 @@ public class DealerTest {
 
 	@Test
 	public void addDealtCardCallsAddOnHand() {
-		sut.addDealtCard(new Card(Card.Value.Ten, Card.Suite.Clubs));
-		Mockito.verify(m_hand).add(Mockito.any());
+		sut.addDealtCard(card);
+		Mockito.verify(m_hand, Mockito.times(1)).add(card);
 	}
 }
