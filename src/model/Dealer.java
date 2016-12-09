6a7b4aec2b1f1d45a6ff020fe2654e662fd12075
Fredrik Olsson <Fredrik Olsson>
2016-12-09 01:14:45 +0100

    GREEN

--- src/model/Dealer.java
+++ src/model/Dealer.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 public class Dealer extends Player{
 	
 	public boolean dealerWins(Player player) {
+		if(this.calcHandValue()>21 && player.calcHandValue()<=22){
+			return false;
+		}
+		if(player.calcHandValue()>21 && this.calcHandValue()<=22){
+			return true;
+		}
 		if(this.calcHandValue() > player.calcHandValue()){
 			return true;
 		} else{
